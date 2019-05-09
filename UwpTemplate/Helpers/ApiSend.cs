3c634cf26c3d6a87f492f9eaac426d1378706c26
義 鈴木 <tadsuzuki@tech.softbank.co.jp>
2019-05-09 15:25:07 +0900

    プロジェクト ファイルを追加します。

---
+++ UwpTemplate/Helpers/ApiSend.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UwpTemplate.Helpers
+{
+    class ApiSend
+    {
+        static HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// API呼び出し
+        /// </summary>
+        /// <param name="url">呼び出し対象URL</param>
+        /// <param name="json">ボディにセットするJson</param>
+        /// <returns></returns>
+        public async Task<T> SendGet<T>(string url, string json = null)
+        {
+            string errMsg;
+
+            try
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+                if (json != null)
+                {
+                    var body = new StringContent(json, Encoding.UTF8, "application/json");
+                    request.Content = body;
+                    request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    request.Content.Headers.ContentType.CharSet = "UTF-8";
+                }
+
+                HttpResponseMessage response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        var result = await content.ReadAsStringAsync();
+                        var res = JsonConvert.DeserializeObject<T>(result);
+                        return res;
+                    }
+                }
+                else
+                {
+                    string result;
+                    using (HttpContent content = response.Content)
+                    {
+                        result = await content.ReadAsStringAsync();
+                    }
+                    errMsg = $@"{response.StatusCode.ToString()} : {result}";
+                }
+
+
+            }
+            catch
+            {
+                throw;
+            }
+
+            throw new Exception(errMsg);
+
+        }
+        
+        /// <summary>
+        /// API呼び出し
+        /// </summary>
+        /// <param name="url">呼び出し対象URL</param>
+        /// <param name="httpMethod">HTTPメソッド</param>
+        /// <param name="dic">ボディにセットするdictionaly</param>
+        /// <returns></returns>
+        public async Task<bool> Send(string url, HttpMethod httpMethod, Dictionary<string, string> dic = null)
+        {
+            string errMsg;
+
+            try
+            {
+                HttpRequestMessage request = new HttpRequestMessage(httpMethod, url);
+                if (dic != null)
+                {
+                    var body = new FormUrlEncodedContent(dic);
+                    request.Content = body;
+                    request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+                    //request.Content.Headers.ContentType.CharSet = "UTF-8";
+                }
+
+                HttpResponseMessage response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    string result;
+                    using (HttpContent content = response.Content)
+                    {
+                        result = await content.ReadAsStringAsync();
+                    }
+                    errMsg = $@"{response.StatusCode.ToString()} : {result}";
+                }
+
+
+            }
+            catch
+            {
+                throw;
+            }
+
+            throw new Exception(errMsg);
+
+        }
+    }
+}
---
+++ UwpTemplate/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace UwpTemplate.ViewModels
+{
+    public class MainPageViewModel : Helpers.Observable
+    {
+
+        public Views.MainPage View { get; private set; } = null;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainPageViewModel()
+        {
+
+        }
+
+        public void Initialize(Views.MainPage mainPage)
+        {
+            View = mainPage;
+        }
+
+        #region Id
+        public string Id
+        {
+            get => this.Id_;
+            set => Set(ref this.Id_, value, nameof(Id));
+        }
+        private string Id_;
+        #endregion Id
+
+        #region List
+        public List<string> List { get; private set; } = new List<string> { "value 1", "value 2" };
+
+        public string Value
+        {
+            get => this.Value_;
+            set
+            {
+                Set(ref this.Value_, value, nameof(Value));
+            }
+        }
+        private string Value_;
+        #endregion Value
+
+
+        #region Output
+        public string Output
+        {
+            get => this.Output_;
+            set => Set(ref this.Output_, value, nameof(Output));
+        }
+        private string Output_;
+        #endregion Output
+
+        private Helpers.RelayCommand OutputTextCommand_;
+        public Helpers.RelayCommand OutputTextCommand
+        {
+            get { return OutputTextCommand_ = OutputTextCommand_ ?? new Helpers.RelayCommand(OutputText); }
+        }
+
+        private void OutputText()
+        {
+            // API呼び出し
+            //try
+            //{
+            //    //string url = $@"https://example.com";
+            //    //var responce = await new Helpers.ApiSend().SendGet<List<Models.Example>>(url);
+            //}
+            //catch (Exception ex)
+            //{
+            //    var msg = new MessageDialog(ex.Message);
+            //    await msg.ShowAsync();
+            //}
+
+            this.Output = $@"{this.Id} : {this.Value}";
+        }
+
+    }
+}
